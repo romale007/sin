fbff6cb475d4e91290ed85b234d15d17ca15f47c
romale007 <romale0819@gmail.com>
2020-11-07 22:00:54 +0300

    1st commit

---
+++ precache-manifest.b8af282cd2f7a7be22537fdc5c585fda.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04a040cfe6f7640d6894",
+    "url": "/sin/css/app.1d4b1da1.css"
+  },
+  {
+    "revision": "1572eadcb4492c3e6727691e39875739",
+    "url": "/sin/img/chair_dark.jpg"
+  },
+  {
+    "revision": "913439f307ee3fbeadfea9be52350bdd",
+    "url": "/sin/img/chairs_workshop.jpg"
+  },
+  {
+    "revision": "53aa897a9fa864c5aaa7a99be40d82af",
+    "url": "/sin/img/kitchen_table.jpg"
+  },
+  {
+    "revision": "517c07c7ea6429ff4bd55ba47607d006",
+    "url": "/sin/img/magazine_table.jpg"
+  },
+  {
+    "revision": "7008cdc90005f1cc4bc633926469d742",
+    "url": "/sin/img/set4chairs_1table.jpg"
+  },
+  {
+    "revision": "f2d80092969df2987b001eec3501fe50",
+    "url": "/sin/img/set_2chairs.jpg"
+  },
+  {
+    "revision": "aa28e46fdeb56f342a05a3e5922fbf2a",
+    "url": "/sin/img/transformer_table1.jpg"
+  },
+  {
+    "revision": "50aeedd87c1aefb9dda3ad0e4a7471c8",
+    "url": "/sin/img/transformer_table2.jpg"
+  },
+  {
+    "revision": "6b82656d999a7ec12b57f0bce7ee3c8f",
+    "url": "/sin/img/white_table_mid.jpg"
+  },
+  {
+    "revision": "c933a29f61930237fd18cb40a36a8715",
+    "url": "/sin/index.html"
+  },
+  {
+    "revision": "dec168167d700744d635",
+    "url": "/sin/js/about.3714231f.js"
+  },
+  {
+    "revision": "04a040cfe6f7640d6894",
+    "url": "/sin/js/app.e594e12f.js"
+  },
+  {
+    "revision": "3f4b6a703ed508b2866e",
+    "url": "/sin/js/chunk-vendors.a58b38b6.js"
+  },
+  {
+    "revision": "e0c17b894842a04fb962aae9a656234e",
+    "url": "/sin/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/sin/robots.txt"
+  }
+]);
